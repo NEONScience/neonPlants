ed3cad77285e09eef9787d94d9f63b08e5c5bb2c
Eric Sokol <sokole@gmail.com>
2023-11-15 07:16:00 -0700

    working on update to stackPlantDiv for new subplotID format

--- dev_scripts/test_script.R
+++ dev_scripts/test_script.R
@@ -9,6 +9,9 @@ allDiv <- loadByProduct(
   package = "basic",
   check.size = FALSE)
 
+saveRDS(allDiv,"dev_scripts/allDiv.RDS")
+allDiv <- readRDS("dev_scripts/allDiv.RDS")
+
 ##extract 1m2 data from list of lists and some processing
 data_1m2 <- allDiv[["div_1m2Data"]]
 ###get 10_100
@@ -17,3 +20,12 @@ data_10_100m2 <- allDiv[["div_10m2Data100m2Data"]]
 data_stacked <- stackPlantDiv(
   div_1m2Data = data_1m2,
   div_10m2Data100m2Data = data_10_100m2)
+
+
+data_stacked %>% names()
+data_stacked$subplotID %>% unique()
+
+plot_32.4.10 <- data_stacked %>% filter(subplotID == "32.4.10")
+plot_32.4.10$taxonID %>% length()
+plot_32.4.10$eventID %>% unique()
+plot_32.4.10$taxonID %>% unique() %>% length()
