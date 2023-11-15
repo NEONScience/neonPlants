0d0c95f4e062ede332a7ae76b659e4d0e031b633
Eric Sokol <sokole@gmail.com>
2023-11-15 12:53:14 -0700

    update stackPlantDiv function to include totalSampledAreaFilter cleaned up package repo

--- R/stackPlantDiv.R
+++ R/stackPlantDiv.R
@@ -1,3 +1,23 @@
+#' @title Reformat old subplotIDs
+#'
+#' @author
+#' Eric Sokol \email{esokol@battelleecology.org} \cr
+#'
+#' @description Helper function to reformat old subplotIDs
+#'
+#' @param x old subplotID (text string)
+#'
+#' @return This function returns a data frame
+reformatSubplotID <- function(x){
+  x_split <- strsplit(x,split = "\\.") %>% unlist()
+  if((length(x_split)==3) & x_split[3]%in%c(1,10,100,400)){
+    return(
+      paste(x_split[c(1,3,2)], collapse = "_"))
+  }else{return(x)}
+}
+
+
+
 #' @title Stack NEON plant diversity data
 #'
 #' @author
@@ -5,10 +25,10 @@
 #'
 #' @description Use this function to aggregate NEON plant presence data to reflect plant species present at each plot scale.
 #'
-#' @import httr XML dplyr
 #'
 #' @param div_1m2Data div_1m2Data table in NEON data produdct (Plant presence and percent cover, DP1.10058.001)
 #' @param div_10m2Data100m2Data div_10m2Data100m2Data table in NEON data produdct (Plant presence and percent cover, DP1.10058.001)
+#' @param totalSampledAreaFilter (integer, options are NA, 1, 10, 100, 400) The plot size for which data are returned. Default (NA) will return data for all plot sizes in the dataset. If you select a plot size, the function will filter the data returned to the desired plot size.
 #'
 #' @return This function returns a data frame
 #'
@@ -31,14 +51,9 @@
 #'   package = "basic",
 #'   check.size = FALSE)
 #'
-#' ##extract 1m2 data from list of lists and some processing
-#' data_1m2 <- allDiv[["div_1m2Data"]]
-#' ###get 10_100
-#' data_10_100m2 <- allDiv[["div_10m2Data100m2Data"]]
-#'
 #' data_stacked <- stackPlantDiv(
-#'   div_1m2Data = data_1m2,
-#'   div_10m2Data100m2Data = data_10_100m2)
+#'   div_1m2Data = allDiv$div_1m2Data,
+#'   div_10m2Data100m2Data = allDiv$div_10m2Data100m2Data)
 #' }
 
 
@@ -51,10 +66,34 @@
 
 stackPlantDiv <- function(
     div_1m2Data = NA,
-    div_10m2Data100m2Data = NA){
+    div_10m2Data100m2Data = NA,
+    totalSampledAreaFilter = NA){
 
-  # library(dplyr)
-  # library(stringr)
+
+  # reformat subplotID
+  div_10m2Data100m2Data <- div_10m2Data100m2Data %>%
+    dplyr::mutate(
+      subplotID_old = subplotID,
+      subplotID = dplyr::case_when(
+        nchar(subplotID) == 2 ~
+          paste0(subplotID,"_100"),
+        grepl("\\.", subplotID) ~
+          sapply(subplotID, reformatSubplotID, USE.NAMES = FALSE),
+        .default = subplotID
+      ))
+
+  div_1m2Data <- div_1m2Data %>%
+    dplyr::mutate(
+      subplotID_old = subplotID,
+      subplotID = dplyr::case_when(
+        nchar(subplotID) == 2 ~
+          paste0(subplotID,"_100"),
+        grepl("\\.", subplotID) ~
+          sapply(subplotID, reformatSubplotID, USE.NAMES = FALSE),
+        .default = subplotID
+      ))
+
+
 
   ###deal with eventID and year - 1m2 data
   #create year column
@@ -132,12 +171,20 @@ stackPlantDiv <- function(
 
   ###preparation
 
-  #make sure subplotID is character (seems like it comes down that way now)
-  div_10m2Data100m2Data$subplotID <- as.character(div_10m2Data100m2Data$subplotID)
+  # #make sure subplotID is character (seems like it comes down that way now)
+  # div_10m2Data100m2Data$subplotID <- as.character(div_10m2Data100m2Data$subplotID)
+
+
 
   #separate the 10m2 data from the 100m2 data:
-  data_100m2 = div_10m2Data100m2Data[which(nchar(div_10m2Data100m2Data$subplotID)<3), ]
-  data_10m2 = div_10m2Data100m2Data[which(nchar(div_10m2Data100m2Data$subplotID)>2), ]
+
+  # data_100m2 = div_10m2Data100m2Data[which(nchar(div_10m2Data100m2Data$subplotID)<3), ]
+  # data_10m2 = div_10m2Data100m2Data[which(nchar(div_10m2Data100m2Data$subplotID)>2), ]
+
+  data_100m2 <- div_10m2Data100m2Data %>%
+    dplyr::filter(grepl("_100$", subplotID))
+  data_10m2 <- div_10m2Data100m2Data %>%
+    dplyr::filter(grepl("_10_",subplotID))
 
   ###build 10m2 data by aggregating observations from 1m2
   # (years sampled both 1 and 10_100) and 10m2
@@ -145,74 +192,90 @@ stackPlantDiv <- function(
   data_10m2Build <- div_1m2Data
 
   #rename 1m2 subplots so associated observations will combine with 10m2 observations
-  data_10m2Build$subplotID[data_10m2Build$subplotID == "31.1.1"] <- "31.1.10"
-  data_10m2Build$subplotID[data_10m2Build$subplotID == "31.4.1"] <- "31.4.10"
-  data_10m2Build$subplotID[data_10m2Build$subplotID == "32.2.1"] <- "32.2.10"
-  data_10m2Build$subplotID[data_10m2Build$subplotID == "32.4.1"] <- "32.4.10"
-  data_10m2Build$subplotID[data_10m2Build$subplotID == "40.1.1"] <- "40.1.10"
-  data_10m2Build$subplotID[data_10m2Build$subplotID == "40.3.1"] <- "40.3.10"
-  data_10m2Build$subplotID[data_10m2Build$subplotID == "41.1.1"] <- "41.1.10"
-  data_10m2Build$subplotID[data_10m2Build$subplotID == "41.4.1"] <- "41.4.10"
+  # data_10m2Build$subplotID[data_10m2Build$subplotID == "31.1.1"] <- "31.1.10"
+  # data_10m2Build$subplotID[data_10m2Build$subplotID == "31.4.1"] <- "31.4.10"
+  # data_10m2Build$subplotID[data_10m2Build$subplotID == "32.2.1"] <- "32.2.10"
+  # data_10m2Build$subplotID[data_10m2Build$subplotID == "32.4.1"] <- "32.4.10"
+  # data_10m2Build$subplotID[data_10m2Build$subplotID == "40.1.1"] <- "40.1.10"
+  # data_10m2Build$subplotID[data_10m2Build$subplotID == "40.3.1"] <- "40.3.10"
+  # data_10m2Build$subplotID[data_10m2Build$subplotID == "41.1.1"] <- "41.1.10"
+  # data_10m2Build$subplotID[data_10m2Build$subplotID == "41.4.1"] <- "41.4.10"
+
+  data_10m2Build <- data_10m2Build %>%
+    dplyr::mutate(subplotID = gsub("_1_", "_10_",subplotID))
 
   #combine what was the nested 1m2 subplot data with the 10m2 data to get the
   # complete list of species in each 10m2 subplot
-  data_10m2 <- rbind(data_10m2, data_10m2Build)
+  data_10m2 <- dplyr::bind_rows(data_10m2, data_10m2Build)
 
   ###aggregate 100m2 data by combining 10m2 observations with 100m2 observations
   #rename 10m2 to combine with 100m2
   data_100m2Build <- data_10m2
 
   # rename 10m2 subplots so associated observations will combine with 100m2 observations
-  data_100m2Build$subplotID[data_100m2Build$subplotID == "31.1.10"] <- 31
-  data_100m2Build$subplotID[data_100m2Build$subplotID == "31.4.10"] <- 31
-  data_100m2Build$subplotID[data_100m2Build$subplotID == "32.2.10"] <- 32
-  data_100m2Build$subplotID[data_100m2Build$subplotID == "32.4.10"] <- 32
-  data_100m2Build$subplotID[data_100m2Build$subplotID == "40.1.10"] <- 40
-  data_100m2Build$subplotID[data_100m2Build$subplotID == "40.3.10"] <- 40
-  data_100m2Build$subplotID[data_100m2Build$subplotID == "41.1.10"] <- 41
-  data_100m2Build$subplotID[data_100m2Build$subplotID == "41.4.10"] <- 41
+  # data_100m2Build$subplotID[data_100m2Build$subplotID == "31.1.10"] <- 31
+  # data_100m2Build$subplotID[data_100m2Build$subplotID == "31.4.10"] <- 31
+  # data_100m2Build$subplotID[data_100m2Build$subplotID == "32.2.10"] <- 32
+  # data_100m2Build$subplotID[data_100m2Build$subplotID == "32.4.10"] <- 32
+  # data_100m2Build$subplotID[data_100m2Build$subplotID == "40.1.10"] <- 40
+  # data_100m2Build$subplotID[data_100m2Build$subplotID == "40.3.10"] <- 40
+  # data_100m2Build$subplotID[data_100m2Build$subplotID == "41.1.10"] <- 41
+  # data_100m2Build$subplotID[data_100m2Build$subplotID == "41.4.10"] <- 41
+
+  data_100m2Build <- data_100m2Build %>%
+    dplyr::mutate(subplotID = gsub("_10_[0-9]","_100", subplotID))
 
   # combine what was the nested 10m2 subplot data with the 100m2 data to get
   # the complete list of species in each 100m2 subplot
-  data_100m2 <- rbind(data_100m2, data_100m2Build)
+  data_100m2 <- dplyr::bind_rows(data_100m2, data_100m2Build)
 
   #################recombine the 1m2 data#####################
-  div_1m2Data <- rbind(div_1m2Data, div_1m2DataOut)
+  div_1m2Data <- dplyr::bind_rows(div_1m2Data, div_1m2DataOut)
 
   #################remove duplicates and combine to one data frame#####################
   ###make sure unique at each scale after combinations Need to figure out how to do unique on specific columns or get rid Different people might have measured the 1 and 10m subplots which could result in otherwise duplicate entries, for example. Maybe have to get rid of the stuff like date above?
-  div_1m2Data <- unique(div_1m2Data)
-  data_10m2 <- unique(data_10m2)
-  data_100m2 <- unique(data_100m2)
+  div_1m2Data <- div_1m2Data %>% dplyr::distinct()
+  data_10m2 <- data_10m2 %>% dplyr::distinct()
+  data_100m2 <- data_100m2 %>% dplyr::distinct()
+
 
   ###create the 400m2 plot species lists
   data_400m2 <- data_100m2
   data_400m2$subplotID <- "400"
-  data_400m2 <- unique(data_400m2)
+  data_400m2 <- data_400m2 %>% dplyr::distinct()
 
   ###this chunk combines the data from all scales of measurement into one table
 
   #data10_100_400 <- rbind(data_10m2, data_100m2)
-  data <- rbind(div_1m2Data, data_10m2, data_100m2, data_400m2)
+  div_1m2Data$totalSampledArea <- 1
+  data_10m2$totalSampledArea <- 10
+  data_100m2$totalSampledArea <- 100
+  data_400m2$totalSampledArea <- 400
+  data <- dplyr::bind_rows(div_1m2Data, data_10m2, data_100m2, data_400m2)
 
   #remove duplicates
-  divPlantPresenceData <- unique(data)
+  divPlantPresenceData <- data %>% dplyr::distinct()
 
   #don't pass target taxa present to larger-scale subplots if not true
-  divplantPresenceData <- divPlantPresenceData %>%
+  divPlantPresenceData <- divPlantPresenceData %>%
     dplyr::group_by(eventID, plotID, subplotID) %>%
-    dplyr::mutate(tot = n()) %>%
+    dplyr::mutate(tot = dplyr::n()) %>%
     dplyr::filter(
       (tot > 1 & targetTaxaPresent != "N") |
         (tot <= 1 & targetTaxaPresent == targetTaxaPresent)) %>%
     dplyr::select(-tot) %>%
     dplyr::ungroup()
 
-  return(divplantPresenceData)
+  # filter to totalSampledAreaFilter if necessary
+  if(!is.na(totalSampledAreaFilter)){
+    if(totalSampledAreaFilter %in% c(1,10,100,400)){
+      divPlantPresenceData <- divPlantPresenceData %>%
+        dplyr::filter(
+          totalSampledArea == totalSampledAreaFilter)
+    }else{
+      message(totalSampledAreaFilter, " is not a valid option for 'totalSampledAreaFilter', returning the full dataset.")
+    }
+  }
 
+  return(divPlantPresenceData)
 }
-
-
-
-
-
--- dev_scripts/test_script.R
+++ dev_scripts/test_script.R
@@ -1,31 +1,49 @@
 # load neonUtilities
 library (neonUtilities)
 
-# get data
-allDiv <- loadByProduct(
-  dpID = "DP1.10058.001",
-  site = "SRER",
-  startdate = "2010-01",
-  package = "basic",
-  check.size = FALSE)
+# install dev version
+devtools::install_github("NEONScience/neonPlants@dev")
+library(neonPlants)
 
-saveRDS(allDiv,"dev_scripts/allDiv.RDS")
+# # get data
+
+# allDiv <- loadByProduct(
+#   dpID = "DP1.10058.001",
+#   site = "SRER",
+#   startdate = "2010-01",
+#   package = "basic",
+#   check.size = FALSE)
+#
+# saveRDS(allDiv,"dev_scripts/allDiv.RDS")
+
 allDiv <- readRDS("dev_scripts/allDiv.RDS")
 
-##extract 1m2 data from list of lists and some processing
-data_1m2 <- allDiv[["div_1m2Data"]]
-###get 10_100
-data_10_100m2 <- allDiv[["div_10m2Data100m2Data"]]
 
+# stack the data
 data_stacked <- stackPlantDiv(
-  div_1m2Data = data_1m2,
-  div_10m2Data100m2Data = data_10_100m2)
+  div_1m2Data = allDiv$div_1m2Data,
+  div_10m2Data100m2Data = allDiv$div_10m2Data100m2Data)
 
 
 data_stacked %>% names()
 data_stacked$subplotID %>% unique()
 
-plot_32.4.10 <- data_stacked %>% filter(subplotID == "32.4.10")
-plot_32.4.10$taxonID %>% length()
-plot_32.4.10$eventID %>% unique()
-plot_32.4.10$taxonID %>% unique() %>% length()
+# stack the data and filter to 10m plot
+data_stacked_10m <- stackPlantDiv(
+  div_1m2Data = allDiv$div_1m2Data,
+  div_10m2Data100m2Data = allDiv$div_10m2Data100m2Data,
+  totalSampledAreaFilter = 10)
+
+data_stacked_10m %>% names()
+data_stacked_10m$subplotID %>% unique()
+
+# stack the data and filter to 10m plot
+data_stacked_10m <- stackPlantDiv(
+  div_1m2Data = allDiv$div_1m2Data,
+  div_10m2Data100m2Data = allDiv$div_10m2Data100m2Data,
+  totalSampledAreaFilter = 34)
+
+data_stacked_10m %>% names()
+data_stacked_10m$subplotID %>% unique()
+
+
