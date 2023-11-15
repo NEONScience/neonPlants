2b5ea234416cf1e29332847528c0a2984bae52c5
Eric Sokol <sokole@gmail.com>
2023-11-15 11:56:07 -0700

    add test script for biomass fxns

---
+++ dev_scripts/test_script_hbp_vst.R
@@ -0,0 +1,7 @@
+############################
+# getVstHbp
+# example specifying non-default arguments
+VstHbpData <- getVstHbp(
+  site = c("HARV","JERC"),
+  start = "2019", end = "2022")
+NEONbiomass()
