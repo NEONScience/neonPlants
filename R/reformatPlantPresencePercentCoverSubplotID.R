a122a5f2fce4699ef35ed4b6a4fd50f195517002
Courtney Meier <cmeier@BattelleEcology.org>
2025-03-12 11:05:42 -0600

    Standardizing Roxygen header text based on CRAN requirements.

--- R/reformatPlantPresencePercentCoverSubplotID.R
+++ R/reformatPlantPresencePercentCoverSubplotID.R
@@ -1,17 +1,23 @@
-#' @title Reformat old subplotIDs
+#' @title Convert old NEON subplotIDs to new format
 #'
-#' @author
-#' Eric Sokol \email{esokol@battelleecology.org} \cr
+#' @author Eric Sokol \email{esokol@battelleecology.org} \cr
 #'
-#' @description Helper function to reformat old subplotIDs
+#' @description Helper function to reformat old subplotIDs. SubplotIDs are updated from "X.X.X" format to "X_X_X" format.
 #'
-#' @param x old subplotID (text string)
+#' @param x Old subplotIDs with "X.X.X" format. [character]
 #'
-#' @return This function returns a data frame
-reformatSubplotID <- function(x){
-  x_split <- strsplit(x,split = "\\.") %>% unlist()
-  if((length(x_split)==3) & x_split[3]%in%c(1,10,100,400)){
-    return(
-      paste(x_split[c(1,3,2)], collapse = "_"))
-  }else{return(x)}
+#' @return Data frame with subplotIDs reformatted to new "X_X_X" style.
+ 
+reformatSubplotID <- function(x) {
+  
+  x_split <- strsplit(x, 
+                      split = "\\.") %>% 
+    unlist()
+  
+  if ((length(x_split) == 3) & x_split[3] %in% c(1, 10, 100, 400)) {
+    
+    return(paste(x_split[c(1, 3, 2)], collapse = "_"))
+    
+  } else {return(x)}
+  
 }
