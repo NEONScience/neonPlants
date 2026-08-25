de9bdfd1c82f27ddffbc31119edd3e3cc859a310
Courtney Meier <cmeier@BattelleEcology.org>
2026-08-25 16:55:58 -0600

    woodProd test development

--- tests/testthat/test_estimateWoodProd.R
+++ tests/testthat/test_estimateWoodProd.R
@@ -6,7 +6,7 @@
 ### Read in test data ####
 vstTestDF <- readRDS(testthat::test_path("testdata", "vst_testDat.rds"))
 
-# Temporary: Reduce to one site for function dev purposes
+#   Reduce to one site to meet function requirements
 theSite <- "ABBY"
 
 map <- vstTestDF$vst_mappingandtagging %>%
@@ -21,26 +21,171 @@ perPlot <- vstTestDF$vst_perplotperyear %>%
 nonWoody <- vstTestDF$`vst_non-woody` %>%
   dplyr::filter(siteID == theSite)
 
-#   Temporary: Further reduce to 2016-2019 to test handling of plots with zero tree biomass when there are no years with trees in the dataset for specific plots
-#--> expect these plots to show up as zeroes
-theEvents <- c("vst_ABBY_2016", "vst_ABBY_2017", "vst_ABBY_2018", "vst_ABBY_2019")
-
-appInd <- appInd %>%
-  dplyr::filter(eventID %in% theEvents)
-
-perPlot <- perPlot %>%
-  dplyr::filter(eventID %in% theEvents)
-
-nonWoody <- nonWoody %>%
-  dplyr::filter(eventID %in% theEvents)
+#   Create test input list
+testInput <- list(vst_mappingandtagging = map,
+                  vst_apparentindividual = appInd,
+                  vst_perplotperyear = perPlot,
+                  `vst_non-woody` = nonWoody)
 
 
 
+### Generate estimateWoodProd outputs with argument defaults ####
+
+woodProdOutputs <- neonPlants::estimateWoodProd(inputDataList = testInput,
+                                                plotSubset = "all",
+                                                flagged = "retain",
+                                                missing = "filter")
 
 
 
-estimateWoodProdOutputs <- estimateWoodProd(inputDataList = VstDat,
-                                            plotSubset = "towerAnnualSubset")
+
+
+
+### Output type tests ####
+
+### Test: Function generates expected output type
+testthat::test_that(desc = "Output type", {
+  testthat::expect_type(object = woodProdOutputs,
+                        type = "list")
+})
+
+
+
+### Tests: Function generates expected output class for all output list objects
+#   Check 'vst_ANPP_indiv' output table is a data frame
+testthat::test_that(desc = "Output class vst_ANPP_indiv", {
+  testthat::expect_s3_class(object = woodProdOutputs$vst_ANPP_indiv,
+                            class = "data.frame")
+})
+
+#   Check 'vst_ANPP_plot' output table is a data frame
+testthat::test_that(desc = "Output class vst_ANPP_plot", {
+  testthat::expect_s3_class(object = woodProdOutputs$vst_ANPP_plot,
+                            class = "data.frame")
+})
+
+#   Check 'vst_ANPP_site' output table is a data frame
+testthat::test_that(desc = "Output class vst_ANPP_site", {
+  testthat::expect_s3_class(object = woodProdOutputs$vst_ANPP_site,
+                            class = "data.frame")
+})
+
+#   Check 'duplicates' output table is a data frame
+testthat::test_that(desc = "Output class duplicates", {
+  testthat::expect_s3_class(object = woodProdOutputs$duplicates,
+                            class = "data.frame")
+})
+
+#   Check 'flagged' output table is a data frame
+testthat::test_that(desc = "Output class flagged", {
+  testthat::expect_s3_class(object = woodProdOutputs$flagged,
+                            class = "data.frame")
+})
+
+#   Check 'missing' output table is a data frame
+testthat::test_that(desc = "Output class missing", {
+  testthat::expect_s3_class(object = woodProdOutputs$missing,
+                            class = "data.frame")
+})
+
+
+
+
+
+
+### Output dimension tests ####
+
+### Test: Function generates data frames with expected dimensions using test data and function defaults
+#--> Defaults: plotSubset = "all", flagged = "retain", missing = "filter"
+
+#   Check expected column number of 'vst_ANPP_indiv' data frame
+testthat::test_that(desc = "Output data frame column number 'vst_ANPP_indiv'", {
+  testthat::expect_identical(object = ncol(woodProdOutputs$vst_ANPP_indiv),
+                             expected = as.integer(36))
+})
+
+#   Check expected row number of 'vst_ANPP_indiv' data frame
+testthat::test_that(desc = "Output data frame row number 'vst_ANPP_indiv'", {
+  testthat::expect_identical(object = nrow(woodProdOutputs$vst_ANPP_indiv),
+                             expected = as.integer(5400))
+})
+
+#   Check expected column number of 'vst_ANPP_plot' data frame
+testthat::test_that(desc = "Output data frame column number 'vst_ANPP_plot'", {
+  testthat::expect_identical(object = ncol(woodProdOutputs$vst_ANPP_plot),
+                             expected = as.integer(11))
+})
+
+#   Check expected row number of 'vst_ANPP_plot' data frame
+testthat::test_that(desc = "Output data frame row number 'vst_ANPP_plot'", {
+  testthat::expect_identical(object = nrow(woodProdOutputs$vst_ANPP_plot),
+                             expected = as.integer(142))
+})
+
+#   Check expected column number of 'vst_ANPP_site' data frame
+testthat::test_that(desc = "Output data frame column number 'vst_ANPP_site'", {
+  testthat::expect_identical(object = ncol(woodProdOutputs$vst_ANPP_site),
+                             expected = as.integer(9))
+})
+
+#   Check expected row number of 'vst_ANPP_site' data frame
+testthat::test_that(desc = "Output data frame row number 'vst_ANPP_site'", {
+  testthat::expect_identical(object = nrow(woodProdOutputs$vst_ANPP_site),
+                             expected = as.integer(9))
+})
+
+#   Check expected column number of 'duplicates' data frame
+testthat::test_that(desc = "Output data frame column number 'duplicates'", {
+  testthat::expect_identical(object = ncol(woodProdOutputs$duplicates),
+                             expected = as.integer(28))
+})
+
+#   Check expected row number of 'duplicates' data frame
+testthat::test_that(desc = "Output data frame row number 'duplicates'", {
+  testthat::expect_identical(object = nrow(woodProdOutputs$duplicates),
+                             expected = as.integer(63))
+})
+
+#   Check expected column number of 'flagged' data frame
+testthat::test_that(desc = "Output data frame column number 'flagged'", {
+  testthat::expect_identical(object = ncol(woodProdOutputs$flagged),
+                             expected = as.integer(32))
+})
+
+#   Check expected row number of 'flagged' data frame
+testthat::test_that(desc = "Output data frame row number 'flagged'", {
+  testthat::expect_identical(object = nrow(woodProdOutputs$flagged),
+                             expected = as.integer(245))
+})
+
+#   Check expected column number of 'missing' data frame
+testthat::test_that(desc = "Output data frame column number 'missing'", {
+  testthat::expect_identical(object = ncol(woodProdOutputs$missing),
+                             expected = as.integer(31))
+})
+
+#   Check expected row number of 'missing' data frame
+testthat::test_that(desc = "Output data frame row number 'missing'", {
+  testthat::expect_identical(object = nrow(woodProdOutputs$missing),
+                             expected = as.integer(31))
+})
+
+
+
+
+
+
+### Error handling tests ####
+
+
+#--> Focus only on those checks within woodProd function...
+### Tests: Generate expected errors for 'inputDataList'
+#   Test 'inputDataList' is a list
+testthat::test_that(desc = "Argument 'inputDataList' is list object", {
+  testthat::expect_error(object = neonPlants::estimateWoodMass(inputDataList = vstTestDF$vst_apparentindividual),
+                         regexp = "Argument 'inputDataList' must be a list object from neonUtilities::loadByProduct()")
+})
+
 
 
 
