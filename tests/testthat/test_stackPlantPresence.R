f79cae0b163fe945037fb1399ea6671a8711b457
Courtney Meier <cmeier@BattelleEcology.org>
2026-09-17 10:15:08 -0600

    Variables added to stackPlantPresence

--- tests/testthat/test_stackPlantPresence.R
+++ tests/testthat/test_stackPlantPresence.R
@@ -2,26 +2,23 @@
 ### POC: Dave Barnett, dbarnett@BattelleEcology.org
 
 ### Read in test data
-testList <- readRDS(testthat::test_path("testdata", "stackPlantPresence_testData.rds"))
+testList <- readRDS(testthat::test_path("testdata", "div_testDat.rds"))
 test_div_1m2Data <- testList$div_1m2Data
 test_div_10m2Data100m2Data <- testList$div_10m2Data100m2Data
+
 pppc <- stackPlantPresence(testList)
 pppcti <- stackPlantPresence(input_1m2Data = test_div_1m2Data,
                              input_10m2Data100m2Data = test_div_10m2Data100m2Data)
 
 
 ### Test: Function generates expected output type
-#   Test list input
-testthat::test_that(desc = "Output type list input", {
-  
+testthat::test_that(desc = "Output type is list", {
+
+  #   Test list input
   testthat::expect_type(object = pppc,
                         type = "list")
-  
-})
 
-#   Test table input
-testthat::test_that(desc = "Output type table input", {
-  
+  #   Test table input
   testthat::expect_type(object = pppcti,
                         type = "list")
 })
@@ -29,17 +26,14 @@ testthat::test_that(desc = "Output type table input", {
 
 
 ### Test: Function generates expected output class
-#   Test list input
 testthat::test_that(desc = "Output class list input", {
-  
-  testthat::expect_s3_class(object = pppc,
-                            class = "data.frame")
-})
 
-#   Test table input
-testthat::test_that(desc = "Output class table input", {
-  
-  testthat::expect_s3_class(object = pppcti,
+  #   Test list input
+  testthat::expect_s3_class(object = pppc$div_stackedPresence,
+                            class = "data.frame")
+
+  #   Test table input
+  testthat::expect_s3_class(object = pppcti$div_stackedPresence,
                             class = "data.frame")
 })
 
@@ -47,28 +41,28 @@ testthat::test_that(desc = "Output class table input", {
 
 ### Test: Function converts old subplotID types correctly
 testthat::test_that(desc = "subplotID contains only the expected values", {
-  
-  # Filter and mutate the first dataframe
+
+  #   Filter and mutate the first dataframe
   filtered_1m2Data <- test_div_1m2Data %>%
     dplyr::filter(uid == "b2313e67-aa8d-48e2-8689-5b2781f9ee68") %>%
-    dplyr::mutate(subplotID = replace(subplotID, 
+    dplyr::mutate(subplotID = replace(subplotID,
                                       subplotID == "31_1_1",
                                       "31.1.1"))
-  
-  # Filter and mutate the second dataframe
+
+  #   Filter and mutate the second dataframe
   filtered_10m2Data <- test_div_10m2Data100m2Data %>%
     dplyr::filter(uid %in% c("bbcbcee8-e8c2-48b0-a1ff-8b65e4fa08f4", "c8876aed-69d9-40d9-9cfe-1927a9340f2a")) %>%
     dplyr::mutate(subplotID = dplyr::case_when(subplotID == "31_10_1" ~ "31.1.10",
                                                subplotID == "31_100" ~ "31",
                                                TRUE ~ subplotID))
-  
-  # Call stackPlantPresence with the filtered dataframes
-  result <- stackPlantPresence(input_1m2Data = filtered_1m2Data, 
+
+  #   Call stackPlantPresence with the filtered dataframes
+  result <- stackPlantPresence(input_1m2Data = filtered_1m2Data,
                                input_10m2Data100m2Data = filtered_10m2Data)
-  
-  # Test that all subplotID values in the result are in the allowed set
+
+  #   Test that all subplotID values in the result are in the allowed set
   testthat::expect_true(
-    all(result$subplotID %in% c("31_1_1", "31_10_1", "31_100", "400")),
+    all(result$div_stackedPresence$subplotID %in% c("31_1_1", "31_10_1", "31_100", "400")),
     info = "Not all values in subplotID are from the allowed set"
   )
 })
@@ -76,35 +70,13 @@ testthat::test_that(desc = "subplotID contains only the expected values", {
 
 
 ### Test: Function generates data frame with expected dimensions using test data
-##  Test list input
-#   Check expected row number of output
-testthat::test_that(desc = "Output data frame row number list input", {
-  
-  testthat::expect_identical(object = nrow(pppc),
+#   Check expected column and row numbers of output
+testthat::test_that(desc = "Output data frame dimensions", {
+
+  testthat::expect_identical(object = nrow(pppc$div_stackedPresence),
                              expected = as.integer(320))
-})
 
-
-#   Check expected column number of output
-testthat::test_that(desc = "Output data frame column number list input", {
-  
-  testthat::expect_identical(object = ncol(pppc),
-                             expected = as.integer(29))
-})
-
-
-##  Test table inputs
-#   Check expected row number of output
-testthat::test_that(desc = "Output data frame row number table input", {
-  
-  testthat::expect_identical(object = nrow(pppcti),
-                             expected = as.integer(320))
-})
-
-#   Check expected column number of output
-testthat::test_that(desc = "Output data frame row number table input", {
-  
-  testthat::expect_identical(object = ncol(pppcti),
+  testthat::expect_identical(object = ncol(pppc$div_stackedPresence),
                              expected = as.integer(29))
 })
 
@@ -113,21 +85,21 @@ testthat::test_that(desc = "Output data frame row number table input", {
 ### Tests: Generate expected errors for 'inputDataList' ####
 #   Test 'inputDataList' is a list
 testthat::test_that(desc = "Argument 'inputDataList' is list object", {
-  
+
   testthat::expect_error(object = stackPlantPresence(inputDataList = test_div_1m2Data),
                          regexp = "Argument 'inputDataList' must be a list object")
 })
 
 #   Test 'inputDataList' contains required tables
 testthat::test_that(desc = "Required tables present in 'inputDataList' input", {
-  
+
   testthat::expect_error(object = stackPlantPresence(inputDataList = testList[1]),
                          regexp = "Required tables missing from 'inputDataList'")
 })
 
 #   Test table inputs are NA if 'inputDataList' supplied
 testthat::test_that(desc = "Table inputs NA when required", {
-  
+
   testthat::expect_error(object = stackPlantPresence(inputDataList = testList,
                                                      input_1m2Data = test_div_1m2Data),
                          regexp = "When 'inputDataList' is supplied all table input arguments must be NA")
@@ -135,7 +107,7 @@ testthat::test_that(desc = "Table inputs NA when required", {
 
 #   Test table inputs are NA if 'inputDataList' supplied
 testthat::test_that(desc = "Table inputs NA when required", {
-  
+
   testthat::expect_error(object = stackPlantPresence(inputDataList = testList,
                                                      input_10m2Data100m2Data = test_div_10m2Data100m2Data),
                          regexp = "When 'inputDataList' is supplied all table input arguments must be NA")
@@ -145,7 +117,7 @@ testthat::test_that(desc = "Table inputs NA when required", {
 
 ### Tests: Generate expected errors with table inputs ####
 testthat::test_that(desc = "Table inputs are data frames when required", {
-  
+
   testthat::expect_error(object = stackPlantPresence(input_1m2Data = testList,
                                                      input_10m2Data100m2Data = test_div_10m2Data100m2Data),
                          regexp = "Data frames must be supplied for all table inputs if 'inputDataList' is missing")
@@ -156,7 +128,7 @@ testthat::test_that(desc = "Table inputs are data frames when required", {
 ### Test: Generate expected errors for issues with div_1m2Data table (works for inputDataList or input_1m2Data source)
 # Test when div_1m2Data lacks required column
 testthat::test_that(desc = "Table 'input_1m2Data' missing column", {
-  
+
   testthat::expect_error(object = stackPlantPresence(input_1m2Data = test_div_1m2Data %>%
                                                       dplyr::select(-taxonID),
                                                      input_10m2Data100m2Data = test_div_10m2Data100m2Data),
@@ -165,7 +137,7 @@ testthat::test_that(desc = "Table 'input_1m2Data' missing column", {
 
 #   Test when div_1m2Data has no data
 testthat::test_that(desc = "Table 'div_1m2Data' missing data", {
-  
+
   testthat::expect_error(object = stackPlantPresence(input_1m2Data = test_div_1m2Data %>%
                                                        dplyr::filter(uid == "coconut"),
                                                      input_10m2Data100m2Data = test_div_10m2Data100m2Data),
@@ -177,7 +149,7 @@ testthat::test_that(desc = "Table 'div_1m2Data' missing data", {
 ### Test: Generate expected errors for issues with div_10m2Data100m2Data table (works for inputDataList or div_10m2Data100m2Data source)
 # Test when div_10m2Data100m2Data lacks required column
 testthat::test_that(desc = "Table 'input_10m2Data100m2Datainput_10m2Data100m2Data' missing column", {
-  
+
   testthat::expect_error(object = stackPlantPresence(input_1m2Data = test_div_1m2Data,
                                                      input_10m2Data100m2Data = test_div_10m2Data100m2Data %>%
                                                        dplyr::select(-taxonID)),
@@ -186,38 +158,47 @@ testthat::test_that(desc = "Table 'input_10m2Data100m2Datainput_10m2Data100m2Dat
 
 #   Test when div_10m2Data100m2Data has no data
 testthat::test_that(desc = "Table 'div_10m2Data100m2Data' missing data", {
-  
+
   testthat::expect_error(object = stackPlantPresence(input_1m2Data = test_div_1m2Data,
                                                      input_10m2Data100m2Data = test_div_10m2Data100m2Data %>%
                                                        dplyr::filter(uid == "doppelganger")),
                          regexp = "Table 'input_10m2Data100m2Data' has no data.")
 })
 
-# Test that raw and processed data contain matching taxa
-taxa1m2 <- test_div_1m2Data$taxonID[-which(is.na(test_div_1m2Data$taxonID))]
+#   Test that raw and processed data contain matching taxa
 testthat::test_that(desc="Same taxa are present in raw and processed data", {
-  testthat::expect_true(object = all(length(setdiff(unique(pppc$taxonID),
-                                                    unique(c(taxa1m2, 
-                                                             test_div_10m2Data100m2Data$taxonID))))==0,
-                                     length(setdiff(unique(c(taxa1m2, 
+
+  taxa1m2 <- test_div_1m2Data$taxonID[-which(is.na(test_div_1m2Data$taxonID))]
+
+  testthat::expect_true(object = all(length(setdiff(unique(pppc$div_stackedPresence$taxonID),
+                                                    unique(c(taxa1m2,
+                                                             test_div_10m2Data100m2Data$taxonID)))) == 0,
+                                     length(setdiff(unique(c(taxa1m2,
                                                              test_div_10m2Data100m2Data$taxonID)),
-                                                    unique(pppc$taxonID)))==0))
+                                                    unique(pppc$div_stackedPresence$taxonID))) == 0))
 })
 
-# Test that each larger area is a superset of the smaller areas
+
+
+### Test that each larger area is a superset of the smaller areas
+
 testthat::test_that(desc="Taxa in 10m2 contain all taxa in 1m2 plots", {
-  testthat::expect_length(object = setdiff(unique(pppc$taxonID[which(pppc$totalSampledArea==1)]), 
-                                           unique(pppc$taxonID[which(pppc$totalSampledArea==10)])),
-  n=0)
-})
-testthat::test_that(desc="Taxa in 100m2 contain all taxa in 10m2 plots", {
-  testthat::expect_length(object = setdiff(unique(pppc$taxonID[which(pppc$totalSampledArea==10)]), 
-                                           unique(pppc$taxonID[which(pppc$totalSampledArea==100)])),
-                          n=0)
-})
-testthat::test_that(desc="Taxa in 400m2 contain all taxa in 100m2 plots", {
-  testthat::expect_length(object = setdiff(unique(pppc$taxonID[which(pppc$totalSampledArea==100)]), 
-                                           unique(pppc$taxonID[which(pppc$totalSampledArea==400)])),
+
+  testthat::expect_length(object = setdiff(unique(pppc$div_stackedPresence$taxonID[which(pppc$div_stackedPresence$totalSampledArea == 1)]),
+                                           unique(pppc$div_stackedPresence$taxonID[which(pppc$div_stackedPresence$totalSampledArea == 10)])),
                           n=0)
 })
 
+testthat::test_that(desc="Taxa in 100m2 contain all taxa in 10m2 plots", {
+
+  testthat::expect_length(object = setdiff(unique(pppc$div_stackedPresence$taxonID[which(pppc$div_stackedPresence$totalSampledArea == 10)]),
+                                           unique(pppc$div_stackedPresence$taxonID[which(pppc$div_stackedPresence$totalSampledArea == 100)])),
+                          n=0)
+})
+
+testthat::test_that(desc="Taxa in 400m2 contain all taxa in 100m2 plots", {
+
+  testthat::expect_length(object = setdiff(unique(pppc$div_stackedPresence$taxonID[which(pppc$div_stackedPresence$totalSampledArea == 100)]),
+                                           unique(pppc$div_stackedPresence$taxonID[which(pppc$div_stackedPresence$totalSampledArea == 400)])),
+                          n=0)
+})
